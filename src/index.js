c1c75519df3e7898e8fecf34132756070ab9a697
bastien17-dev <bastien.paquier@gmail.com>
2020-03-14 13:29:37 +0100

    responsive and organize files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './script/App';
+import App from './scripts/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
